9880163cc1e25728ad66ccf9f71eedbfef5aeaa4
pureum <lpm0831@naver.com>
2023-11-21 00:56:30 +0900

    [refactoring] baseFragment 적용 - RecordRecyclerFragment

--- presentation/src/main/java/com/running/runningmate2/ui/fragment/RecordRecyclerFragment.kt
+++ presentation/src/main/java/com/running/runningmate2/ui/fragment/RecordRecyclerFragment.kt
@@ -1,13 +1,11 @@
 package com.running.runningmate2.ui.fragment
 
 import android.graphics.Color
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.running.runningmate2.R
+import com.running.runningmate2.base.BaseFragment
 import com.running.runningmate2.bottomSheet.DetailBottomsheet
 import com.running.runningmate2.databinding.FragmentRecordRecyclerBinding
 import com.running.runningmate2.recyclerView.Adapter
@@ -15,34 +13,31 @@ import com.running.runningmate2.model.Data
 import com.running.runningmate2.model.toData
 import com.running.runningmate2.viewModel.activityViewModel.MainViewModel
 
-class RecordRecyclerFragment : Fragment() {
+class RecordRecyclerFragment : BaseFragment<FragmentRecordRecyclerBinding>(R.layout.fragment_record_recycler) {
     private val mainViewModel: MainViewModel by activityViewModels()
-    private val adapter = Adapter()
-    val binding: FragmentRecordRecyclerBinding by lazy{
-        FragmentRecordRecyclerBinding.inflate(layoutInflater)
+    private val adapter by lazy { Adapter() }
+
+    override fun initData() {
+        mainViewModel.readDB()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mainViewModel.readDB()
+    override fun initUI() {
         binding.myRecyclerView.apply {
             this.adapter = this@RecordRecyclerFragment.adapter
             this.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         }
+    }
 
+    override fun initListener() {
         adapter.setItemClickListener(object: Adapter.OnItemClickListener{
             override fun onClick(data: Data) {
                 val bottomDialogSheet = DetailBottomsheet(data)
                 bottomDialogSheet.show(parentFragmentManager, bottomDialogSheet.tag)
             }
         })
-        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun initObserver() {
         mainViewModel.runningData.observe(viewLifecycleOwner) { data ->
             if(data.size > 0){
                 binding.myRecyclerView.minimumHeight = 875
@@ -57,7 +52,6 @@ class RecordRecyclerFragment : Fragment() {
                 binding.noData.visibility=View.VISIBLE
                 binding.noData.text = "기록 데이터가 없어요.."
             }
-
         }
     }
 }
